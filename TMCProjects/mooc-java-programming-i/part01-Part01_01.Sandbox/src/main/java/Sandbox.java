2248e23e7ded6453a100ce6f7e231cc841eec4ba
alex.dopico@gmail.com <Aficionada13>
2020-12-17 18:12:58 +0100

    first commit - Ada Lovelace Exercise

---
+++ TMCProjects/mooc-java-programming-i/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,9 @@
+/** Use this sandbox to exercise to try out different
+ * things later on.
+ */
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Write your program here
+    }
+}
---
+++ TMCProjects/mooc-java-programming-i/part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
@@ -0,0 +1,15 @@
+/** <h1>Part 1 - Ada Lovelace</h1>
+ * <p>Add a new line below the line comment that prints the
+ * String "Ada Lovelace" and run the program.</p>
+ *
+ * @author: Alex Dopico
+ * @version: 1.0.0
+ * @date: 17/12/2020
+ */
+
+class AdaLovelace {
+
+    public static void main(String[] args) {
+        System.out.println("Ada Lovelace");
+    }
+}
