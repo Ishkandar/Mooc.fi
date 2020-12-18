359a93946de16276fefc013140db201f43ed913d
alex.dopico@gmail.com <Aficionada13>
2020-12-18 02:36:44 +0100

    v1.1.0 - added javadocs

--- TMCProjects/mooc-java-programming-i/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ TMCProjects/mooc-java-programming-i/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -1,8 +1,14 @@
-/** Use this sandbox to exercise to try out different
+/**
+ * Use this sandbox to exercise to try out different
  * things later on.
  */
 public class Sandbox {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         // Write your program here
     }
--- TMCProjects/mooc-java-programming-i/part01-Part01_01.Sandbox/src/test/java/SandboxTest.java
+++ TMCProjects/mooc-java-programming-i/part01-Part01_01.Sandbox/src/test/java/SandboxTest.java
@@ -3,12 +3,21 @@ import fi.helsinki.cs.tmc.edutestutils.MockStdio;
 import fi.helsinki.cs.tmc.edutestutils.Points;
 import org.junit.*;
 
+/**
+ * The type Sandbox test.
+ */
 @Points("01-01")
 public class SandboxTest {
 
+    /**
+     * The Io.
+     */
     @Rule
     public MockStdio io = new MockStdio();
 
+    /**
+     * Free points.
+     */
     @Test
     public void freePoints() {
     }
