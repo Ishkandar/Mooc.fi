95d346dbc1bd83d420d3b5444d49e4a96f53c611
alex.dopico@gmail.com <Aficionada13>
2020-12-19 18:25:57 +0100

    Added 'Message three times' and 'Greeting'

--- TMCProjects/mooc-java-programming-i/part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
+++ TMCProjects/mooc-java-programming-i/part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
@@ -1,3 +1,13 @@
+/**<h1>Part 1 - Message Three Times</h1>
+ * <p>Write a program that asks the user to write a string.
+ * When the user has given a string (that is, written some
+ * text and pressed enter), the program must print the
+ * user's string three times.
+ *
+ * @author Alex Dopico
+ * @version 1.0.0
+ * @date 19/12/2020
+ */
 
 import java.util.Scanner;
 
@@ -7,7 +17,11 @@ public class MessageThreeTimes {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Write a message:");
-        // Write your program here
+        String userInput = scanner.nextLine();
+
+        System.out.println(userInput);
+        System.out.println(userInput);
+        System.out.println(userInput);
 
     }
 }
--- TMCProjects/mooc-java-programming-i/part01-Part01_08.Greeting/src/main/java/Greeting.java
+++ TMCProjects/mooc-java-programming-i/part01-Part01_08.Greeting/src/main/java/Greeting.java
@@ -1,3 +1,13 @@
+/**<h1>Part 1 - Greeting</h1>
+ * <p>Write a program that prompts the user for their
+ * name their name with the message "What's your name?".
+ * When the user has written their name, the program has
+ * to print "Hi", followed by the user's name.
+ *
+ * @author Alex Dopico
+ * @version 1.0.0
+ * @since 19/12/2020
+ */
 
 import java.util.Scanner;
 
@@ -5,8 +15,8 @@ public class Greeting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
-
+        System.out.println("What's your name?");
+        String userName = scanner.nextLine();
+        System.out.printf("Hi %s", userName);
     }
 }
