f99c0bfad45e8171cbc9df2672dda5fc042b9658
alex.dopico@gmail.com <Aficionada13>
2020-12-19 18:09:30 +0100

    added Hi Ada Lovelace

---
+++ TMCProjects/mooc-java-programming-i/part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
@@ -0,0 +1,17 @@
+/**<h1>Part 1 - Hi Ada Lovelace!</h1>
+ * <p>Modify the program so that it prints the content
+ * of the variable <i>name</i>, and the printed text is
+ * in its fully form.</p>
+ *
+ * @author Alex Dopico
+ * @version 1.0.0
+ * @date 19/12/2020
+ */
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        System.out.printf("Hi %s%n", name);
+
+    }
+}
